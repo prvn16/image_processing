d1d823f066b8f567c37d2fbc0a7a7da6d8cfe832
oldoak <oldoak.hn@gmail.com>
2016-01-06 22:52:54 +0000

    Tidy up of code and addition of jpg file

--- PixelIntensity/PixelIntensity.C
+++ PixelIntensity/PixelIntensity.C
@@ -18,8 +18,11 @@ int main(int argc, char **argv)
 
     namedWindow(argv[1], WINDOW_AUTOSIZE);
     namedWindow(argv[2], WINDOW_AUTOSIZE);
+    namedWindow(argv[3], WINDOW_AUTOSIZE);
 
     cvtColor(in_image, out_image, COLOR_BGR2GRAY);
+    imshow(argv[1], in_image);
+    imshow(argv[2], out_image);
 
     Size image_sz = in_image.size();
     int rows = in_image.rows;
@@ -27,18 +30,16 @@ int main(int argc, char **argv)
     rows = image_sz.height;
     cols = image_sz.width;
     
-    float intense = atoi(argv[3]);
+    float intense = atoi(argv[4]);
     for(int r {0}; r < rows; ++r)
     {
         for(int c {0}; c < cols; ++c)
         {
-            Scalar intensity = out_image.at<uchar>(r, c);
             out_image.at<uchar>(r, c) /= intense;
         }
     }
 
-    imshow(argv[1], in_image);
-    imshow(argv[2], out_image);
+    imshow(argv[3], out_image);
 
     cout << "Press any key to exit...\n";
     waitKey();
