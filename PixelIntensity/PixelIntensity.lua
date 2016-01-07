bb6c6f8da0036dafcf3fb3e68d1bcdd850374bf7
oldoak <oldoak.hn@gmail.com>
2016-01-07 13:11:37 +0000

    Updates to .lua file for premake

--- PixelIntensity/PixelIntensity.lua
+++ PixelIntensity/PixelIntensity.lua
@@ -1,35 +1,41 @@
 -- sample_premake.lua
 solution "PixelIntensity"
-   configurations { "Debug", "Release", "Machine" }
+    configurations { "Debug", "Release", "Machine" }
  
-   project "PixelIntensity"
-      kind "ConsoleApp"
-      language "C++"
-      objdir "${HOME}/src/cpp/image_processing/obj/%{cfg.buildcfg}"
-      targetdir "${HOME}/src/cpp/image_processing/bin/%{cfg.buildcfg}"
+    objdir "${HOME}/src/cpp/image_processing/obj/%{cfg.buildcfg}"
+    targetdir "${HOME}/src/cpp/image_processing/bin/%{cfg.buildcfg}"
 
-      files { "**.H", "**.C" }
+    configuration "Debug"
+        defines { "_DEBUG" }
+        flags { "Symbols" }
+        targetsuffix "_g"
  
-      configuration "Debug"
-         defines { "_DEBUG" }
-         flags { "Symbols" }
-         targetsuffix "_g"
- 
-      configuration "Release"
-         defines { "_REL" }
-         flags { "Optimize" }
+    configuration "Release"
+        defines { "_REL" }
+        flags { "Optimize" }
 
-      configuration "Machine"
-         defines { "_MACHINE" }
-         flags { "Symbols" }
-         targetsuffix "_m"
+    configuration "gmake"
+        buildoptions { "-Wall" }
+        buildoptions { "-fPIC" }
+        buildoptions { "-pthread" }
+        buildoptions { "-std=c++14" }
 
-      configuration "gmake"
-         buildoptions { "-Wall" }
-         buildoptions { "-fPIC" }
-         buildoptions { "-pthread" }
-         buildoptions { "-std=c++14" }
+    includedirs { "/usr/include", 
+                 "/usr/local/include/opencv", 
+                 "/usr/local/include/opencv2" }
+    libdirs { "/usr/lib", 
+                "/usr/local/lib" }
+    links { "opencv_core", 
+           "opencv_imgproc", 
+           "opencv_imgcodecs", 
+           "opencv_highgui" }
 
-      includedirs { "/usr/include", "/usr/local/include/opencv", "/usr/local/include/opencv2" }
-      libdirs { "/usr/lib", "/usr/local/lib" }
-      links { "opencv_core", "opencv_imgproc", "opencv_imgcodecs", "opencv_highgui" }
+    project "PixelIntensityBW"
+        kind "ConsoleApp"
+        language "C++"
+        files { "PixelIntensityBW.C" }
+
+    project "PixelIntensityColor"
+        kind "ConsoleApp"
+        language "C++"
+        files { "PixelIntensityColor.C" }
