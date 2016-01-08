9c4e72998ff577c91a574e6dbf7d2c3ff8a88902
oldoak <oldoak.hn@gmail.com>
2016-01-08 09:54:23 +0000

    Code completion for blurring

--- ImageSpatialAverage/ImageSpatialAverage.C
+++ ImageSpatialAverage/ImageSpatialAverage.C
@@ -14,41 +14,15 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    Size image_sz = in_image.size();
     namedWindow(argv[1], WINDOW_AUTOSIZE);
     imshow(argv[1], in_image);
 
-    int rows = in_image.rows;
-    int cols = in_image.cols;
-    rows = image_sz.height;
-    cols = image_sz.width;
     Mat out_image;
-    
-    for(int r {0}; r < rows; ++r)
-    {
-        for(int c {0}; c < cols; ++c)
-        {
-             for(int i {0}; i < 3; ++i)
-             {
-                 int total {in_image.at<Vec3b>(r,c)[i]};
-                 cout << (int) in_image.at<Vec3b>(r,c+1)[i] << endl;
-                 //if(in_image.at<Vec3b>(r,c-1)[i]) total += in_image.at<Vec3b>(r,c)[i];
-                 if(in_image.at<Vec3b>(r,c+1)[i]) total += (int) in_image.at<Vec3b>(r,c)[i];
-                 //if(in_image.at<Vec3b>(r-1,c-1)[i]) total += in_image.at<Vec3b>(r,c)[i];
-                 //if(in_image.at<Vec3b>(r+1,c)[i]) total += in_image.at<Vec3b>(r,c)[i];
-                 //if(in_image.at<Vec3b>(r-1,c)[i]) total += in_image.at<Vec3b>(r,c)[i];
-                 //if(in_image.at<Vec3b>(r+1,c)[i]) total += in_image.at<Vec3b>(r,c)[i];
-                 //if(in_image.at<Vec3b>(r+1,c+1)[i]) total += in_image.at<Vec3b>(r,c)[i];
-                 //if(in_image.at<Vec3b>(r+1,c-1)[i]) total += in_image.at<Vec3b>(r,c)[i];
-                 //in_image.at<Vec3b>(r,c)[i] = total/10;
-                 //    saturate_cast<uchar>(in_image.at<Vec3b>(r,c)[i]);
-             }
-        }
-    }
+    int kernel_size = atoi(argv[2]);
+    blur(in_image, out_image, Size(kernel_size, kernel_size));
 
-    out_image = in_image;
     namedWindow("Altered Image", WINDOW_AUTOSIZE);
-    imshow("Altered Image", in_image);
+    imshow("Altered Image", out_image);
 
     cout << "Press any key to exit...\n";
     waitKey();
